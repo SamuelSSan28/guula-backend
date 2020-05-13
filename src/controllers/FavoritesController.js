54efd8200288ae57528c9792e34f37578ad806a9
Gisele <giselesousar@gmail.com>
2020-05-13 14:01:19 -0300

    Add favorites sem testes

---
+++ src/controllers/FavoritesController.js
@@ -0,0 +1,46 @@
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async index(request, response){
+
+        const usuario_id = request.headers.authorization;
+
+        const [count] = await connection('receitas_usuarios').where('usuario_id', usuario_id).count();
+        response.header('X-total-count', count['count(*)']);
+
+        const incidents = await connection('receitas')
+            .join('receitas_usuarios', 'receitas.id', '=', 'receitas_usuarios.receita_id')
+            .select([
+                'receitas.*', 
+            ] );
+
+        return response.json(incidents);
+    },
+
+    async create(request, response){
+        const { receita_id } = request.body;
+        const usuario_id = request.headers.authorization;
+
+        const [id] = await connection('receitas_usuarios').insert({
+            usuario_id,
+            receita_id
+        })
+
+        return response.json({ id })
+    },
+
+    async delete(request, response){
+        const { id } = request.params;
+        const usuario_id = request.headers.authorization;
+
+        const receita_usuario = await connection('receitas_usuarios').where('id', id).select('usuario_id').first();
+
+        if(receita_usuario.usuario_id != usuario_id){
+            return response.status(401).json({ error: 'operation not permitted' });
+        }
+
+        await connection('receitas_usuarios').where('id', id).delete();
+        return response.status(204).send();
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
 const UserController = require('./controllers/UserController');
-
+const FavoritesController = require('./controllers/FavoritesController');
 
 const routes = express.Router();
 
-/**
-routes.get('/users', UserController.index);
-routes.post('/users', UserController.create);
- */
+routes.get('/favorites', FavoritesController.index);
+routes.post('/favorites', FavoritesController.create);
+routes.delete('/favorites', FavoritesController.delete);
 module.exports = routes; //exportando as rotas
